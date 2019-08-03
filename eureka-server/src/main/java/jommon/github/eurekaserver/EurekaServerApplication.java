b18b25640953c294960b500aa11280ea9c30316d
huangjiangjun <huangjiangjun@ebaolife.com>
2019-08-03 20:03:42 +0800

    init project

---
+++ eureka-server/src/main/java/jommon/github/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,13 @@
+package jommon.github.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+}
